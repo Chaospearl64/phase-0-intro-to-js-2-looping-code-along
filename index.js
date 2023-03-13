1ed592dc60be5059dd486b25b1b0c3f78603f727
Cody Cogbill <codeman24@gmail.com>
2023-03-13 14:11:15 -0600

    Completed Loop lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+let emptyArr = []
+function writeCards(Arr, Event) {
+    for (let i = 0; i < Arr.length; i++) {
+        emptyArr.push(`Thank you, ${Arr[i]}, for the wonderful ${Event} gift!`)
+    }
+    console.log(emptyArr)
+    return emptyArr
+}
+writeCards([Guadalupe, Ollie, Aki], "surprise")
+
+//===============================
+
+function countDown(num) {
+    while (num >= 0) {
+        console.log(num)
+        --num
+    }
+}
+countDown(10)
